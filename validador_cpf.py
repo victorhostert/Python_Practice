6fd88ddf56dbc6287aef2976f91d570d657bf42f
victorhostert <victorwhostert@gmail.com>
2021-09-01 09:54:11 -0300

    added cpf verifier

---
+++ validador_cpf.py
@@ -0,0 +1,23 @@
+cpf = input('Digite um CPF, sem a máscara')
+novo_cpf = cpf[:-2]
+reverso = 10
+total = 0
+for index in range(19):
+    if index > 8:
+        index -= 9
+    
+    total += int(novo_cpf[index]) * reverso
+
+    reverso -= 1
+    if reverso < 2:
+        reverso = 11
+        d = 11 - (total % 11)
+        if d > 9:
+            d = 0
+        total = 0
+        novo_cpf += str(d)
+
+if novo_cpf == cpf:
+    print('CPF é válido')
+else:
+    print('CPF inválido')
